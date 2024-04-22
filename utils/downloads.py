0122f137df98436e40da1f54f95f56353c74e044
Bruno Azuma Balzano <brunoazuma@usp.br>
2024-04-22 22:00:24 +0000

    Adicionado notebook com estimativa populacional

--- utils/downloads.py
+++ utils/downloads.py
@@ -3,6 +3,10 @@ from geopandas import (
     read_file,
     GeoDataFrame,
 )
+from pandas import (
+    DataFrame,
+    read_excel,
+)
 from logging import (
     Logger,
     getLogger,
@@ -15,6 +19,7 @@ from os.path import (
 from os import makedirs
 from urllib.parse import urlparse
 from urllib.request import urlretrieve
+from zipfile import ZipFile
 
 class Censo(Enum):
    CENSO_2010=2010
@@ -23,6 +28,21 @@ class Censo(Enum):
 class Nivel(Enum):
    DISTRITOS='distritos'
    SETORES='setores'
+
+def __prepare_cache(url:str, file_dir:str, logger:Logger=getLogger()) -> str:
+
+    parsed_url = urlparse(url)
+    filename = basename(parsed_url.path)
+    file_path = join(file_dir, filename)
+
+    if not exists(file_path):
+        logger.info(f'Baixando o arquivo {filename} de {url}.')
+        makedirs(file_dir, exist_ok=True)
+        urlretrieve(url, file_path)
+    else:
+        logger.info(f'O arquivo {file_path} já foi baixado anteriormente. Usando cache local.')
+
+    return file_path
 
 def get_malha_url(censo:Censo, nivel:Nivel) -> str:
     """
@@ -80,18 +100,9 @@ def download_malha(censo:Censo, nivel:Nivel, filtro:str=None, logger:Logger=getL
         GeoDataFrame com os dados escolhidos.
     """
     url = get_malha_url(censo, nivel)
-
-    parsed_url = urlparse(url)
-    filename = basename(parsed_url.path)
     file_dir =join(cache_dir, nivel.value, str(censo.value))
-    file_path = join(file_dir, filename)
-
-    if not exists(file_path):
-        logger.info(f'Baixando a malha de {nivel.value} do censo de {censo.value} de {url}.')
-        makedirs(file_dir, exist_ok=True)
-        urlretrieve(url, file_path)
-    else:
-        logger.info(f'O arquivo {file_path} já foi baixado anteriormente. Usando cache local.')
+    logger.info(f'Carregando a malha de {nivel.value} do censo de {censo.value}.')
+    file_path = __prepare_cache(url, file_dir, logger=logger)
 
     gdf = read_file(file_path, **kwargs)
 
@@ -99,4 +110,29 @@ def download_malha(censo:Censo, nivel:Nivel, filtro:str=None, logger:Logger=getL
         gdf = gdf.query(filtro)
 
     return gdf
-        
+
+def get_dados_url(censo:Censo, nivel:Nivel) -> str:
+    if censo == Censo.CENSO_2010:
+        if nivel == Nivel.SETORES:
+            return 'https://ftp.ibge.gov.br/Censos/Censo_Demografico_2010/Resultados_do_Universo/Agregados_por_Setores_Censitarios/SP_Capital_20231030.zip'
+
+
+def download_dados(censo:Censo, nivel:Nivel, arquivo:str=None, filtro:str=None, logger:Logger=getLogger(), cache_dir:str='data/cache/', **kwargs) -> DataFrame:
+    url = get_dados_url(censo, nivel)
+    file_dir =join(cache_dir, nivel.value, str(censo.value))
+    logger.info(f'Carregando a malha de {nivel.value} do censo de {censo.value}.')
+    file_path = __prepare_cache(url, file_dir, logger=logger)
+
+    df = None
+    if censo == Censo.CENSO_2010 and arquivo != None:
+        with ZipFile(file_path) as z:
+            with z.open(f'Base informaçoes setores2010 universo SP_Capital/EXCEL/{arquivo}') as f:
+                df = read_excel(f, thousands='.', decimal=',', dtype={'Cod_setor': str})
+
+    if df is None:
+        return None
+
+    if filtro:
+        df = df.query(filtro)
+
+    return df
